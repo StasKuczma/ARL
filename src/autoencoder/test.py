aebf2e12c0cefbd324a9ef12bb812070642c9520
Stanislaw <staskuczma@gmail.com>
2024-12-12 16:35:54 +0100

    feat: C propeller test | add plot

--- src/autoencoder/test.py
+++ src/autoencoder/test.py
@@ -14,14 +14,14 @@ import os
 
 input_dim = 6  
 model = Autoencoder(input_dim)
-model.load_state_dict(torch.load('./autoencoder_model.pth'))
+model.load_state_dict(torch.load('./models/C_propeller/autoencoder_model.pth'))
 model.eval()
 
-scaler_params = np.load('./scaler_params.npy', allow_pickle=True)
+scaler_params = np.load('./models/C_propeller/scaler_params.npy', allow_pickle=True)
 scaler_mean, scaler_scale = scaler_params
 scaler = lambda x: (x - scaler_mean) / scaler_scale
 
-with open('threshold.json', 'r') as f:
+with open('models/C_propeller/threshold.json', 'r') as f:
     data = json.load(f)
     threshold = data['threshold']
 
@@ -35,11 +35,16 @@ i=0
 correct_detection = 0
 incorrect_detection = 0
 
+TP = 0
+TN = 0
+FP = 0
+FN = 0
+
 for csv_file in csv_files:
     file_path = os.path.join(folder_path, csv_file)
     
     df_test = pd.read_csv(file_path)
-    A_propeler_test = df_test[['A_aX', 'A_aY', 'A_aZ', 'A_gX', 'A_gY', 'A_gZ']]
+    A_propeler_test = df_test[['C_aX', 'C_aY', 'C_aZ', 'C_gX', 'C_gY', 'C_gZ']]
 
     data_test = scaler(A_propeler_test.values)
     data_test_tensor = torch.tensor(data_test, dtype=torch.float32)
@@ -60,33 +65,60 @@ for csv_file in csv_files:
 
     filename_without_extension = csv_file.split('.')[0]
 
-    # Split the filename by underscores
     parts = filename_without_extension.split('_')
 
-    # Get the last part (which contains '2000')
     last_part = parts[-1]
 
-    # Get the character at the desired position (index 1 for '2' in '2000')
-    desired_character = last_part[0]
+    desired_character = last_part[2]
 
     if damage > no_damage:
         print(f"Damage detected in {csv_file}")
 
         if desired_character == '1' or desired_character == '2':
             correct_detection += 1
+            TP += 1
         else:
             incorrect_detection += 1
+            FP += 1
     else:
         print(f"No damage detected in {csv_file}")
         if desired_character == '0':
             correct_detection += 1
+            TN += 1
         else:
             incorrect_detection += 1
+            FN += 1
 
 print('Correct detection:', correct_detection)
 print('Incorrect detection:', incorrect_detection)
 
 print('Accuracy:', correct_detection / (correct_detection + incorrect_detection))
+
+print('True Positives:', TP)
+print('True Negatives:', TN)
+print('False Positives:', FP)
+print('False Negatives:', FN)
+
+confusion_matrix = np.array([[TP, FN],
+                             [FP, TN]])
+
+labels = ['Uszkodzenie', 'Brak uszkodzenia']
+
+fig, ax = plt.subplots()
+cax = ax.matshow(confusion_matrix, cmap=plt.cm.Blues)
+
+plt.colorbar(cax)
+
+ax.set_xticklabels([''] + labels)
+ax.set_yticklabels([''] + labels)
+
+plt.xlabel('Predykcja')
+plt.ylabel('Rzetywista wartość')
+
+for (i, j), val in np.ndenumerate(confusion_matrix):
+    ax.text(j, i, f'{val}', ha='center', va='center', color='black')
+
+plt.show()
 
 
 
